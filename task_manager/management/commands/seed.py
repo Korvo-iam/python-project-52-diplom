46c68401d9bcfda2669324bbad50ab4e12ee93d7
Korvo-Iam <emilaubakirov@gmail.com>
2026-04-02 17:22:14 +0500

    172202042026

--- task_manager/management/commands/seed.py
+++ task_manager/management/commands/seed.py
@@ -12,23 +12,25 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         # -------------------- USERS --------------------
         users_data = [
-            {"username": "admin", "first_name": "Admin", "last_name": "User", "is_superuser": True, "is_staff": True},
-            {"username": "User1", "first_name": "Иван", "last_name": "Иванов"},
-            {"username": "User2", "first_name": "Мария", "last_name": "Петрова"},
-            {"username": "User3", "first_name": "Алексей", "last_name": "Сидоров"},
+            {"username": "admin", "first_name": "Admin", "last_name": "User", "is_superuser": True, "is_staff": True, "password": "admin123"},
+            {"username": "User1", "first_name": "Иван", "last_name": "Иванов", "password": "123"},
+            {"username": "User2", "first_name": "Мария", "last_name": "Петрова", "password": "123"},
+            {"username": "User3", "first_name": "Алексей", "last_name": "Сидоров", "password": "123"},
         ]
+
         created_users = {}
+
         for u in users_data:
-            user, created = User.objects.get_or_create(username=u["username"], defaults=u)
+            user, created = User.objects.get_or_create(username=u["username"])
             if created:
+                user.first_name = u["first_name"]
+                user.last_name = u["last_name"]
                 if u.get("is_superuser"):
-                    user.set_password("admin123")
-                    user.save()
-                    self.stdout.write(f"Created superuser {u['username']}/admin123")
-                else:
-                    user.set_password("123")
-                    user.save()
-                    self.stdout.write(f"Created user {u['username']}/123")
+                    user.is_superuser = True
+                    user.is_staff = True
+                user.set_password(u["password"])
+                user.save()
+                self.stdout.write(f"Created user {u['username']}/{u['password']}")
             created_users[u["username"]] = user
 
         admin_user = created_users["admin"]
@@ -63,7 +65,6 @@ class Command(BaseCommand):
                     executor=user,
                     deadline=timezone.now() + timezone.timedelta(days=random.randint(1, 10))
                 )
-                # случайные метки (1-3 на задачу)
                 task.labels.set(random.sample(labels, k=random.randint(1, 3)))
                 task.save()
                 self.stdout.write(f"Created task for {username}")
